84b65ada5ba6fc04b4d0f0ef79aa105bb6db8c80
Alex Frank <alexfrankcodes@gmail.com>
2020-11-30 16:13:10 -0800

    Created a basic route

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => res.send("Hello world!"));
+
 app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}`)
 );
