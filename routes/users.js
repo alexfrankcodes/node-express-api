55b1911814b68e87f4bbb75148938d8a8b3a032a
Alex Frank <alexfrankcodes@gmail.com>
2020-12-01 14:23:46 -0800

    Added automatic unique ID for every new user posted to mock database

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,26 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
 const users = [
-  {
-    firstName: "Alex",
-    lastName: "Frank",
-    age: 24,
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 26,
-  },
+  // {
+  //   firstName: "Alex",
+  //   lastName: "Frank",
+  //   age: 24,
+  // },
+  // {
+  //   firstName: "Jane",
+  //   lastName: "Doe",
+  //   age: 26,
+  // },
 ];
 
 router.get("/", (req, res) => res.send(users));
 
 router.post("/", (req, res) => {
   const newUser = req.body;
-  users.push(newUser);
+  users.push({ ...newUser, id: uuidv4() });
   res.send(
     `User ${newUser.firstName} ${newUser.lastName} has been added to the database.`
   );
