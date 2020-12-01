33b0a6f247457d722c39fefde01a27a201dddf0f
Alex Frank <alexfrankcodes@gmail.com>
2020-11-30 16:26:13 -0800

    Implemented ability to add new users to mock database

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,27 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("Users route"));
+const users = [
+  {
+    firstName: "Alex",
+    lastName: "Frank",
+    age: 24,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 26,
+  },
+];
+
+router.get("/", (req, res) => res.send(users));
+
+router.post("/", (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  res.send(
+    `User ${newUser.firstName} ${newUser.lastName} has been added to the database.`
+  );
+});
 
 export default router;
